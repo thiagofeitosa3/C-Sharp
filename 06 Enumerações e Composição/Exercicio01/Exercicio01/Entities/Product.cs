ee225525af23cafb5f94765f10cf27702666019f
ThiagoDev <thiagofeitosadearaujo@outlook.com>
2024-10-28 14:09:50 -0400

    novo exercicio em composição'

---
+++ 06 Enumerações e Composição/Exercicio01/Exercicio01/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio01.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+
+
+
+
+
+}
