0ba7449c68432fdc65771c1f37f9b8d16ae242da
ThiagoDev <thiagofeitosadearaujo@outlook.com>
2024-11-05 14:26:32 -0400

    Exercicio novo em polimorfismo

---
+++ 07 Herança e Polimorfismo/Exercicio2/Exercicio2/Entities/TaxPayer.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio2.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public TaxPayer() 
+        {
+        }
+
+        protected TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+    }
+}
